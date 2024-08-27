8fa77c92a80dbe3153d217129218b285076e5bfd
EdRiver8 <dwein08@gmail.com>
2024-08-26 20:40:08 -0500

    Eliminado Logico y Editar
    Realizacion de la logica para el eliominado logico y la funcion de editar empleados

--- js/6.3-clase-modulo-empleados.js
+++ js/6.3-clase-modulo-empleados.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // si existen datos, los convirte a objetos js y llama la funcion para mostrar en la tabla
   if (empleadosAlmacenados) {
     empleados = JSON.parse(empleadosAlmacenados); // para recibir data y convertirla a tipo js
-    actualizarTabla();
+    actualizarTablaLogica();
   }
 });
 
@@ -29,10 +29,11 @@ function agregarEmpleado(
     cedula: cedula,
     valorHora: salarioBasico / 188,
     edad: edad,
+    esEmpleado: true,
   }; // construye un objeto de tipo empleado
   empleados.push(empleado); // lo agrega al final
   localStorage.setItem("empleados", JSON.stringify(empleados)); // para enviar data
-  actualizarTabla(); // actualizamos la vista
+  actualizarTablaLogica(); // actualizamos la vista
 }
 
 function agregarEmpleadoUI() {
@@ -62,6 +63,7 @@ function agregarEmpleadoUI() {
 function limpiarFormulario() {
   document.getElementById("nombre").value = "";
   document.getElementById("nombre").value = "";
+  document.getElementById("nombre").cedula = "";
   document.getElementById("genero").value = "Masculino";
   document.getElementById("edad").value = "";
   document.getElementById("salarioBasico").value = "";
@@ -122,12 +124,43 @@ function actualizarTabla() {
         <button class="edit-btn" onclick="editarEmpleado(${index})">
           <i class="fas fa-edit"></i>
         </button>
-        <button class="delete-btn" onclick="eliminarEmpleado(${index})">
+        <button class="delete-btn" onclick="eliminarEmpleadoLogico(${index})">
           <i class="fas fa-trash-alt"></i>
         </button>
       </td>
     `;
     tbody.appendChild(fila);
+  });
+}
+
+function actualizarTablaLogica() {
+  const tbody = document.getElementById("tablaEmpleados");
+  tbody.innerHTML = "";
+  empleados.forEach((empleado, index) => {
+    // los atributos empleado es cada empleado en la lista de empleados y el index es la posicion o indice del empleado
+    if (empleado.esEmpleado) {
+      let fila = document.createElement("tr");
+      fila.innerHTML = `
+      <td>${empleado.indice}</td>
+      <td>${empleado.nombre}</td>
+      <td>${empleado.genero}</td>
+      <td>${empleado.cedula}</td>
+      <td>${empleado.edad}</td>
+      <td>${empleado.valorHora ? empleado.valorHora.toFixed(2) : "0.00"}</td>
+      <td>${
+        empleado.salarioBasico ? empleado.salarioBasico.toFixed(2) : "0.00"
+      }</td>
+      <td class="action-buttons">
+        <button class="edit-btn" onclick="editarEmpleado(${index})">
+          <i class="fas fa-edit"></i>
+        </button>
+        <button class="delete-btn" onclick="eliminarEmpleadoLogico(${index})">
+          <i class="fas fa-trash-alt"></i>
+        </button>
+      </td>
+    `;
+      tbody.appendChild(fila);
+    }
   });
 }
 
@@ -148,6 +181,14 @@ const eliminarEmpleado = function (index) {
   }
 };
 
+const eliminarEmpleadoLogico = function (index) {
+  if (confirm("Estas seguro de eliminar al empleado?")) {
+    empleados[index].esEmpleado = false; // obtenemos el empleado y modificamos su camp esEmpleado a false
+    localStorage.setItem("empleados", JSON.stringify(empleados)); // cuando la llave ya existe (emplados), la actualiza
+    actualizarTablaLogica();
+  }
+};
+
 // implementar un eliminado logico
 
 function editarEmpleado(index) {
@@ -159,6 +200,46 @@ function editarEmpleado(index) {
   document.getElementById("cedula").value = empleado.cedula;
   document.getElementById("edad").value = empleado.edad;
   document.getElementById("salarioBasico").value = empleado.salarioBasico;
+
+  // Modificar el letrero del boton = Actualizar
+  let btnAgregar = document.getElementById("btn-agregar");
+  btnAgregar.textContent = "Actualizar";
+
+  btnAgregar.onclick = function () {
+    actualizarEmpleado(index);
+  };
 }
 
-// finalizar el editar
+function actualizarEmpleado(index) {
+  // 1. Leer los datos del formulario
+  const nombre = document.getElementById("nombre").value;
+  const genero = document.getElementById("genero").value;
+  const cedula = document.getElementById("cedula").value;
+  const edad = document.getElementById("edad").value;
+  const salarioBasico = document.getElementById("salarioBasico").value;
+
+  //2. validar que todos los datos del form esten completos
+  // TODO: refactorizar las sgtes lineas de codigo para que sean una funcion
+  if (!nombre || !genero || !cedula || !edad || !salarioBasico) {
+    alert("Por favor, complete todos los campos.");
+    return;
+  }
+
+  //3. Actualizar la informacion del empleado
+  empleados[index] = {
+    ...empleados[index], // traiga el empleado con todos sus atributos
+    nombre: nombre,
+    genero: genero,
+    cedula: cedula,
+    edad: parseInt(edad) || 0,
+    valorHora: (parseFloat(salarioBasico) || 0) / 188,
+  };
+
+  //4. Actulicemos la base de datos
+  localStorage.setItem("empleados", JSON.stringify(empleados));
+  actualizarTablaLogica();
+  limpiarFormulario();
+
+  //5. Actualizar el boton de nuevo a 'Agregar'
+  document.getElementById("btn-agregar").textContent = "Agregar";
+}
